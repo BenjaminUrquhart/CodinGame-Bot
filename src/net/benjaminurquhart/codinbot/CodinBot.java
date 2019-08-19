93d1a84805e4468229d101772a52854fd7a8c48d
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2019-08-19 17:24:11 -0400

    Added ratelimits

--- src/net/benjaminurquhart/codinbot/CodinBot.java
+++ src/net/benjaminurquhart/codinbot/CodinBot.java
@@ -3,6 +3,7 @@ package net.benjaminurquhart.codinbot;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
 
 import net.benjaminurquhart.jch.CommandHandler;
 import net.dv8tion.jda.core.JDA;
@@ -24,6 +25,13 @@ public class CodinBot {
 	}
 	public static void main(String[] args) throws Exception {
 		JDA jda = new JDABuilder(token).setEnabledCacheFlags(EnumSet.noneOf(CacheFlag.class)).build().awaitReady();
-		jda.addEventListener(new CommandHandler<CodinBot>(new CodinBot(),jda.getSelfUser().getAsMention()+" ",null,"net.benjaminurquhart.codinbot.commands"));
+		CommandHandler<CodinBot> handler = new CommandHandler<>(
+				new CodinBot(),
+				jda.getSelfUser().getAsMention()+" ",
+				"273216249021071360",
+				"net.benjaminurquhart.codinbot.commands"
+		);
+		handler.setRatelimit(1, TimeUnit.SECONDS);
+		jda.addEventListener(handler);
 	}
 }
